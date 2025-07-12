74b6f4bffac21f3069b989c447d08acdde0ad07b
ricassio costa <ricassiocosta.me@gmail.com>
2025-07-12 14:11:10 -0300

    feat: add health_cache_ttl setting to configuration

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Settings:
     redis_url: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
     pp_default: str = os.getenv("PAYMENT_PROCESSOR_URL_DEFAULT")
     pp_fallback: str = os.getenv("PAYMENT_PROCESSOR_URL_FALLBACK")
+    health_cache_ttl: int = 5
 
 @lru_cache
 def get_settings() -> Settings:
