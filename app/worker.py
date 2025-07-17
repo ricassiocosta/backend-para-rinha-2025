1897551e3d1a347b1b83378d7dfc8dcbf79c4c3e
ricassio costa <ricassiocosta.me@gmail.com>
2025-07-16 22:44:15 -0300

    Update worker version logging to include version number

--- app/worker.py
+++ app/worker.py
@@ -3,6 +3,8 @@ import asyncio
 from app.queue import consume_loop
 from app.processor import choose_and_send
 
+_VERSION = "v0.7.1"
+
 async def handle_item(item: dict):
     try:
         await choose_and_send(item["correlationId"], item["amount"])
@@ -11,5 +13,5 @@ async def handle_item(item: dict):
         raise e
 
 if __name__ == "__main__":
-    print("Worker started.")
+    print(f"Worker version {_VERSION} started")
     asyncio.run(consume_loop(handle_item))
